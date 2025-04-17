111c087d4757f4dbb68568c40c5d546f6c7ee114
lriberon <leo.riberon-piatyszek@datadome.co>
2025-04-17 15:09:31 +0200

    fix: move the load of the JS Tag to be in head tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,40 @@
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import { getLayout } from '@vercel/examples-ui';
-import type { LayoutProps } from '@vercel/examples-ui/dist/layout';
-import { DATADOME_JS, DATADOME_TAGS } from '@datadome/module-nextjs';
-import '@vercel/examples-ui/globals.css';
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { getLayout } from "@vercel/examples-ui";
+import type { LayoutProps } from "@vercel/examples-ui/dist/layout";
+import { DATADOME_JS, DATADOME_TAGS } from "@datadome/module-nextjs";
+import "@vercel/examples-ui/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const Layout = getLayout<LayoutProps>(Component);
+  const Layout = getLayout<LayoutProps>(Component);
 
-    return (
-        <Layout
-            path="edge-middleware/bot-protection-datadome"
-            deployButton={{
-                env: ['NEXT_PUBLIC_DATADOME_CLIENT_SIDE_KEY', 'DATADOME_SERVER_SIDE_KEY'],
-            }}
-        >
-            <Component {...pageProps} />
+  const jsTagOptions = `
+    window.ddoptions = {
+      endpoint: '${DATADOME_JS}',
+    };
+  `;
 
-            {/* datadome bot protection */}
-            <Script strategy="lazyOnload" id="load-datadome">{`
-        window.ddjskey = '${process.env.NEXT_PUBLIC_DATADOME_CLIENT_SIDE_KEY}'
-        window.ddoptions = {
-          endpoint: '${DATADOME_JS}'
-        }
-      `}</Script>
-            <Script src={DATADOME_TAGS} strategy="lazyOnload" />
-        </Layout>
-    );
+  return (
+    <>
+      {/* Scripts globaux pour DataDome */}
+      <Script id="jstag-options" strategy="beforeInteractive">
+        {jsTagOptions}
+      </Script>
+      <Script src={DATADOME_TAGS} strategy="beforeInteractive" />
+
+      <Layout
+        path="edge-middleware/bot-protection-datadome"
+        deployButton={{
+          env: [
+            "NEXT_PUBLIC_DATADOME_CLIENT_SIDE_KEY",
+            "DATADOME_SERVER_SIDE_KEY",
+          ],
+        }}
+      >
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
 
 export default MyApp;
