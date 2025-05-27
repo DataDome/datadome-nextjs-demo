f50e9eb8d01ee1fb11e1a06a2b3e3abd26e05cf4
Léo Riberon-Piatyszek <leo.riberon-piatyszek@datadome.co>
2025-05-27 15:00:06 +0200

    feat: initial commit (#1)

---
+++ middleware.ts
@@ -0,0 +1,46 @@
+import { type NextRequest,  NextResponse } from 'next/server';
+import { DataDomeMiddleware } from '@datadome/module-nextjs';
+
+export const config = {
+    /**
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * 
+     * It avoids to send those requests to the DataDome's Middleware.
+     * @see {@link https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher}
+     */
+    matcher: ['/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
+};
+
+const ddMiddleware = new DataDomeMiddleware(process.env.DATADOME_SERVER_SIDE_KEY ?? '');
+
+async function datadomeMiddleware(req: NextRequest, res?: NextResponse) {
+    const { pathname } = req.nextUrl;
+
+    if (pathname === '/omit') {
+        return NextResponse.next();
+    }
+
+    // Force the page to be blocked by DataDome
+    if (pathname === '/blocked') {
+        req.headers.set('user-agent', 'BLOCKUA');
+    }
+    // `datadome.handleRequest(req)` returns a promise that resolves to
+    // a respones (NextResponse) or undefined
+    //
+    // If there's a response, we made a Datadome request.
+    //
+    // If the response has a rewrite, it means the request
+    // was blocked and we should return it, this would
+    // rewrite to the captcha page of Datadome
+    //
+    // If there's no rewrite, you're not a bot and we
+    // send the response that includes Datadome's headers.
+    return ddMiddleware.handleRequest(req, res);
+}
+
+export default async function middleware(req: NextRequest) {
+    return datadomeMiddleware(req);
+}
---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import type { AppProps } from "next/app";
+import { DataDomeComponent } from "@datadome/module-nextjs";
+import { getLayout } from "@vercel/examples-ui";
+import type { LayoutProps } from "@vercel/examples-ui/dist/layout";
+import "@vercel/examples-ui/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const Layout = getLayout<LayoutProps>(Component);
+
+  return (
+    <>
+      {/* DataDome bot protection */}
+      <DataDomeComponent clientSideKey={process.env.NEXT_PUBLIC_DATADOME_CLIENT_SIDE_KEY!} />
+
+      <Layout
+        path="/"
+        deployButton={{
+          env: [
+            "NEXT_PUBLIC_DATADOME_CLIENT_SIDE_KEY",
+            "DATADOME_SERVER_SIDE_KEY",
+          ],
+        }}
+      >
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
+}
+
+export default MyApp;
